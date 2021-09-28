ae61dd5c5be3367b9060b7217f5b01aa3091e3a7
mrc-laufire <mrc@laufire.in>
2021-09-28 09:27:38 +0000

    Added cars model.

---
+++ migrations/20210928051959-create-cars.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('cars', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      make: {
+        type: Sequelize.STRING
+      },
+      model: {
+        type: Sequelize.STRING
+      },
+      vehicleNumber: {
+        unique: true,
+        type: Sequelize.STRING
+      },
+      purchaseDate: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('cars');
+  }
+};
